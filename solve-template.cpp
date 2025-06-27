33e52a8701879b6f5a64e574e34e60681f8adaf3
LashaSD <lashakardava@gmail.com>
2025-06-27 22:11:07 +0400

    add template file

---
+++ solve-template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#ifndef ONLINE_JUDGE
+#include <dbg.h>
+#else
+#define dbg(...) (void)0
+#endif
+
+using namespace std;
+using ll = long long;
+
+void solve()
+{
+
+}
+
+int T = 1;
+int32_t main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // cin >> T;
+    while(T--)
+        solve();
+    return 0;
+}
